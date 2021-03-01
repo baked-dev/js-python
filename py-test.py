eb052ff34dd0c3795603e9c0af786b69d04883da
xhalfbakedguy <tristancilltseinleben@gmail.com>
2021-03-02 00:49:39 +0100

    added json for message passing

--- py-test.py
+++ py-test.py
@@ -1,48 +1,56 @@
 import sys
 import time
+import random
+import json
+import string
 from threading import Thread
 
 class Task(Thread):
     def __init__(self, group=None, target=None, name=None, args=(), kwargs=None):
         super(Task, self).__init__(group=group, target=target, name=name)
-        self.args = args
         self.id = args[0]
-        self.kwargs = kwargs
+        self.task = json.loads(kwargs[0])
         return
 
     def run(self):
-        time.sleep(2)
-        self.send_response(data=['pong', self.kwargs[1]])
+        if self.task['command'] == 'test':
+            return self.do_test()
+        elif self.task['command'] == 'random':
+            return self.do_random()
         return
 
+    def do_test(self):
+        time.sleep(self.task['args'][1])
+        self.send_response({
+            "result": self.task['args'][0]
+        })
+
+    def do_random(self):
+        time.sleep(2)
+        self.send_response({
+            "result": ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(self.task['args'][0]))
+        })
+
     def send_response(self, data):
-        data_string = "|".join(data) 
-        print(f"{self.id}|response|{data_string}")
+        data_string = json.dumps(data) 
+        print(f"{self.id}|1|{data_string}")
         sys.stdout.flush()
         return
 
 def main():
-    try:
-        time.sleep(2)
-        print('0|ready|')
-        sys.stdout.flush()
-        buff = ''
-        i = 0
-        while True:
-            buff += sys.stdin.read(1)
-            if buff.endswith('\n'):
-                input = buff[:-1]
-                buff = ''
-                kwargs = input.strip().split('|')
-                arg = kwargs.pop(0)
-                t = Task(args=(arg,), kwargs=kwargs)
-                t.start()
-                i = i + 1
-
-    except KeyboardInterrupt:
-        sys.stdout.flush()
-        pass
-
+    time.sleep(2)
+    print('0|0')
+    sys.stdout.flush()
+    buff = ''
+    while True:
+        buff += sys.stdin.read(1)
+        if buff.endswith('\n'):
+            input = buff[:-1]
+            buff = ''
+            kwargs = input.strip().split('|')
+            arg = kwargs.pop(0)
+            t = Task(args=(arg,), kwargs=kwargs)
+            t.start()
     return
 
 if __name__ == '__main__':
